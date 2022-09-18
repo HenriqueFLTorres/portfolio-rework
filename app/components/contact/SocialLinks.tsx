8b319c2971e4228090a311772dc16e830fc1ac82
Henrique Torres <ricalisboa@gmail.com>
2022-09-17 21:56:54 -0300

    Added copy label to discord link and Refactored SocialLinks component

--- app/components/contact/SocialLinks.tsx
+++ app/components/contact/SocialLinks.tsx
@@ -7,68 +7,122 @@ import {
   StackOverflow,
   LeetCode
 } from 'public/SocialLinks';
-
-const socialLinksIcons = [
-  {
-    component: LinkedIn,
-    name: 'LinkedIn',
-    linkTo: 'https://www.linkedin.com/in/henriquefltorres/'
-  },
-  {
-    component: GitHub,
-    name: 'GitHub',
-    linkTo: 'https://github.com/HenriqueFLTorres'
-  },
-  {
-    component: Email,
-    name: 'Gmail',
-    linkTo: 'mailto:henriquefltorres@gmail.com'
-  },
-  {
-    component: Blog,
-    name: 'Personal Blog',
-    linkTo: 'https://simplifiedknowledge.vercel.app/'
-  },
-  {
-    component: Discord,
-    name: 'Treffy#4964',
-    copy: true
-  },
-  {
-    component: StackOverflow,
-    name: 'Stack Overflow',
-    linkTo: 'https://stackoverflow.com/users/17776680/treffy'
-  },
-  {
-    component: LeetCode,
-    name: 'LeetCode',
-    linkTo: 'https://leetcode.com/HenriqueFLTorres/'
-  }
-];
+import { useState } from 'react';
+import SmallLabel from '../Shared/SmallLabel';
 
 const SocialLinks = () => {
+  const [showLabel, setShowLabel] = useState(false);
+
   return (
     <div
       id='contact'
       className='flex flex-col w-max mx-auto gap-6 mt-48 mb-36 xl:mb-52'
     >
       {socialLinksIcons.map((link) => (
-        <a
+        <SocialFragment
           key={link.name}
-          className='group w-48 xl:w-64 flex flex-row items-center rounded hover:bg-primary/10 px-5 py-4 cursor-pointer transition-[background-color] duration-400'
-          href={link.linkTo}
-          onClick={() =>
-            !link.linkTo && navigator.clipboard.writeText(link.name)
-          }
-        >
-          <link.component className='fill-primary/50 group-hover:fill-primary h-8 w-8 xl:h-12 xl:w-12 mr-2 xl:mr-6 group-hover:drop-shadow-[0_0_1px_rgba(255,255,255,0.8)] xl:group-hover:drop-shadow-[0_0_3px_rgba(255,255,255,0.8)] transition-[fill,_filter] duration-400' />
-          <p className='text-primary/50 group-hover:text-primary xl:text-xl font-semibold group-hover:drop-shadow-[0_0_1px_rgba(255,255,255,0.8)] xl:group-hover:drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-[color,_filter] duration-400'>
-            {link.name}
-          </p>
-        </a>
+          link={link}
+          showLabel={showLabel}
+          setShowLabel={setShowLabel}
+        />
       ))}
     </div>
   );
 };
+
+const SocialFragment = ({
+  link,
+  showLabel,
+  setShowLabel
+}: {
+  link: socialLinkTypes;
+  showLabel: boolean;
+  setShowLabel: React.Dispatch<boolean>;
+}) => {
+  const { name, copy, linkTo, Component } = link;
+
+  const handleCopy = () => {
+    if (!copy) return;
+
+    navigator.clipboard.writeText(name);
+    setShowLabel(true);
+    setTimeout(() => {
+      setShowLabel(false);
+    }, 2000);
+  };
+
+  return (
+    <a
+      key={name}
+      className={`group w-48 xl:w-64 flex flex-row items-center justify-center rounded ${
+        showLabel && copy ? 'bg-primary/10' : ''
+      } hover:bg-primary/10 px-5 py-4 cursor-pointer transition-[background-color] duration-400`}
+      href={linkTo}
+      onClick={handleCopy}
+    >
+      <Component className='fill-primary/50 group-hover:fill-primary h-8 w-8 xl:h-12 xl:w-12 mr-2 xl:mr-6 group-hover:drop-shadow-[0_0_1px_rgba(255,255,255,0.8)] xl:group-hover:drop-shadow-[0_0_3px_rgba(255,255,255,0.8)] transition-[fill,_filter] duration-400' />
+      <p className='text-primary/50 mr-auto group-hover:text-primary xl:text-xl font-semibold group-hover:drop-shadow-[0_0_1px_rgba(255,255,255,0.8)] xl:group-hover:drop-shadow-[0_0_2px_rgba(255,255,255,0.8)] transition-[color,_filter] duration-400'>
+        {name}
+      </p>
+      {copy && (
+        <SmallLabel
+          title='Copiado'
+          additionalStyling={`translate-y-14 group-hover:translate-y-14 ${
+            showLabel
+              ? 'opacity-100 group-hover:opacity-100'
+              : 'opacity-0 group-hover:opacity-0'
+          }`}
+        />
+      )}
+    </a>
+  );
+};
+
+const socialLinksIcons = [
+  {
+    Component: LinkedIn,
+    name: 'LinkedIn',
+    linkTo: 'https://www.linkedin.com/in/henriquefltorres/'
+  },
+  {
+    Component: GitHub,
+    name: 'GitHub',
+    linkTo: 'https://github.com/HenriqueFLTorres'
+  },
+  {
+    Component: Email,
+    name: 'Gmail',
+    linkTo: 'mailto:henriquefltorres@gmail.com'
+  },
+  {
+    Component: Blog,
+    name: 'Personal Blog',
+    linkTo: 'https://simplifiedknowledge.vercel.app/'
+  },
+  {
+    Component: Discord,
+    name: 'Treffy#4964',
+    copy: true
+  },
+  {
+    Component: StackOverflow,
+    name: 'Stack Overflow',
+    linkTo: 'https://stackoverflow.com/users/17776680/treffy'
+  },
+  {
+    Component: LeetCode,
+    name: 'LeetCode',
+    linkTo: 'https://leetcode.com/HenriqueFLTorres/'
+  }
+];
+
+interface socialLinkTypes {
+  Component: (
+    props: JSX.IntrinsicAttributes & React.SVGProps<SVGSVGElement>
+  ) => JSX.Element;
+  name: string;
+  linkTo?: string;
+  copy?: boolean;
+}
 
 export default SocialLinks;
