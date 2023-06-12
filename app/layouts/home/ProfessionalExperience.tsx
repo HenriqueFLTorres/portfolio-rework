f32f2eda0cd50d1bae13767725069de60aed350d
Henrique Torres <ricalisboa@gmail.com>
2023-06-12 20:59:39 -0300

    feat - updated professional experiences

--- app/layouts/home/ProfessionalExperience.tsx
+++ app/layouts/home/ProfessionalExperience.tsx
@@ -2,13 +2,17 @@ import { useEnglishLanguage } from '@hooks/LanguageContext';
 
 import { BriefCase } from 'public/Emojis';
 
-import portugueseList from '@data/ProfessionalExperience/pt.json';
 import englishList from '@data/ProfessionalExperience/en.json';
+import portugueseList from '@data/ProfessionalExperience/pt.json';
 
 import SmallLabel from '@components/Shared/SmallLabel';
+import { Link } from '@remix-run/react';
+import { cn } from '@utils/classNames';
 import { technologiesList } from 'public/technologies';
+import { useState } from 'react';
 
 const ProfessionalExperience = () => {
+  const [selectedIndex, setSelected] = useState(0);
   const { isEnglish } = useEnglishLanguage();
 
   const getListByLanguage = () => {
@@ -19,7 +23,7 @@ const ProfessionalExperience = () => {
   return (
     <section
       id='experience'
-      className='relative w-full mx-auto max-w-[1200px] small:w-4/5 small:px-0 mb-72'
+      className='relative flex flex-col w-full mx-auto max-w-[1200px] small:w-4/5 gap-4 small:px-0 mb-72'
     >
       <h3 className='flex flex-col items-center gap-4 md:gap-0 md:flex-row xl:h-52 justify-center font-bold text-center md:text-left text-4xl xl:text-5xl mb-20 text-primary mt-36 p-4'>
         <img
@@ -31,46 +35,62 @@ const ProfessionalExperience = () => {
           {isEnglish ? 'Professional Experience' : 'Experiência Profissional'}
         </p>
       </h3>
-      {getListByLanguage().map((experience: experienceType) => {
-        const { company, type, start, end, responsibilities, role, stackUsed } =
-          experience;
 
-        const renderList = technologiesList.filter((technology) =>
-          stackUsed.includes(technology.name)
-        );
-
-        return (
-          <div
+      <div className='flex gap-4 items-center max-w-[600px] w-full mx-auto'>
+        {getListByLanguage().map(({ company }: experienceType, idx) => (
+          <button
+            aria-label={company.name}
             key={company.name}
-            className='flex flex-col items-center gap-4 max-w-[600px] w-full mx-auto p-4'
+            onClick={() => setSelected(idx)}
+            className={cn(
+              'group w-40 xl:w-52 flex group text-primary/50 hover:text-primary flex-row items-center justify-center rounded bg-neutral-700/20 hover:bg-primary/10 px-4 py-3 cursor-pointer transition-[background-color] duration-400 select-none',
+              { ['bg-primary/10 text-primary']: selectedIndex === idx }
+            )}
           >
-            <a
-              className={
-                'group w-40 xl:w-52 flex flex-row items-center justify-center rounded bg-neutral-700/20 hover:bg-primary/10 px-4 py-3 mr-auto cursor-pointer transition-[background-color] duration-400 select-none'
-              }
-              href='https://www.linkedin.com/company/xaza/'
-              target={'_blank'}
-              rel='noreferrer'
+            <img
+              src={company.logo}
+              alt=''
+              className='h-6 w-6 mr-4 inline group-first:invert'
+            />
+            <p className='mr-auto xl:text-xl font-semibold transition-[color,_filter] duration-400'>
+              {company.name}
+            </p>
+          </button>
+        ))}
+      </div>
+
+      <ul className='flex max-w-[600px] w-full mx-auto'>
+        {getListByLanguage().map((experience: experienceType, idx) => {
+          if (selectedIndex !== idx) return <></>;
+
+          const {
+            company,
+            type,
+            start,
+            end,
+            responsibilities,
+            role,
+            stackUsed
+          } = experience;
+
+          const renderList = technologiesList.filter((technology) =>
+            stackUsed.includes(technology.name)
+          );
+
+          return (
+            <li
+              key={company.name}
+              className='flex flex-col justify-between w-full h-max bg-neutral-800/50 rounded-lg px-6 py-4 gap-4 p-4'
             >
-              <img
-                src={company.logo}
-                alt=''
-                className='h-6 w-6 mr-4 inline invert'
-              />
-              <p className='text-primary/50 mr-auto group-hover:text-primary xl:text-xl font-semibold transition-[color,_filter] duration-400'>
-                {company.name}
-              </p>
-            </a>
-            <div className='flex flex-col justify-between w-full h-max bg-neutral-800/50 rounded-lg px-6 py-4 gap-4'>
               <div className='flex flex-col sm:flex-row gap-4 justify-between'>
                 <span className='text-white font-light text-xl'>
                   <h3 className='font-semibold'>{role}</h3>
                   <h5 className='text-secondary'>
-                    {company.name} · {type}
+                    <Link to={company.href} target='_blank'>{company.name}</Link> · {type}
                   </h5>
                 </span>
                 <p className='text-secondary font-light text-lg'>
-                  {start} · {end ?? isEnglish ? 'Present' : 'Presente'}
+                  {start} · {end ? end : isEnglish ? 'Present' : 'Presente'}
                 </p>
               </div>
               <div className='text-white font-light text-base'>
@@ -94,10 +114,10 @@ const ProfessionalExperience = () => {
                   </span>
                 ))}
               </footer>
-            </div>
-          </div>
-        );
-      })}
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
@@ -105,7 +125,7 @@ const ProfessionalExperience = () => {
 export default ProfessionalExperience;
 
 interface experienceType {
-  company: { name: string; logo: string };
+  company: { name: string; logo: string, href: string };
   role: string;
   type: string;
   start: string;
